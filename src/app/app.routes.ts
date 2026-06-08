996ebb88deed5a49a164e30224c886db9a5c3f8a
admin.local <samuel.schmid@bbzbl-it.ch>
2026-06-08 13:54:48 +0200

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guard/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
+    path: 'machines',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/machines/machines').then((m) => m.Machines),
+  },
+  {
+    path: 'accessory-sets',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/accessory-sets/accessory-sets').then((m) => m.AccessorySets),
+  },
+  {
+    path: 'categories',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/categorys/categorys').then((m) => m.Categorys),
+  },
+  { path: '**', redirectTo: '' },
+];
